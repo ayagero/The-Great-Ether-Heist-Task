fc232690ad88431ff7bfe543483f7d4ce795f606
ayagero <ayagero2020@gmail.com>
2025-05-09 23:49:52 +0100

    Update SelfDestruct.sol

--- contracts/SelfDestruct.sol
+++ contracts/SelfDestruct.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract SelfDestruct {
+    constructor(address payable _etherVault) payable {
+        require(msg.value == 0.0005 ether, "Must send exactly 0.0005 ether");
+        selfdestruct(_etherVault);
+    }
+}
